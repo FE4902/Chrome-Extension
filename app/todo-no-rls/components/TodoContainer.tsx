a1bf12464820e4de514284ffbf398eb1f4e7838b
FE4902 <isdn000123@gmail.com>
2024-06-02 18:35:49 +0900

    AppRouter, API 설정

---
+++ app/todo-no-rls/components/TodoContainer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { getTodos } from "@/apis/todos-no-rls";
+
+const TodoContainer = () => {
+    useEffect(() => {
+        getTodos();
+    }, []);
+    return <div>TodoContainer</div>;
+};
+
+export default TodoContainer;
