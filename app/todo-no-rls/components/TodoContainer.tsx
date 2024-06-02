71b9b8c52c83f079c0b7f6505a6cddf37c1b16e1
FE4902 <isdn000123@gmail.com>
2024-06-02 21:13:53 +0900

    UI 작업

--- app/todo-no-rls/components/TodoContainer.tsx
+++ app/todo-no-rls/components/TodoContainer.tsx
@@ -1,15 +1,33 @@
 "use client";
-
 import React, { useEffect } from "react";
 import useTodosController from "../hooks/useTodosController";
+import TodoList from "@/components/ui/TodoList";
 
 const TodoContainer = () => {
-    const { loading, todos } = useTodosController();
+    const {
+        loading,
+        todos,
+        onCreateEmptyTodos,
+        onDeleteTodos,
+        onSearchTodos,
+        onUpdateTodos,
+    } = useTodosController();
 
-    console.log(">>loading", loading);
-    console.log(">>todos", todos);
-
-    return <div>TodoContainer</div>;
+    return (
+        <div>
+            <TodoList
+                sharedUserFullName="test user"
+                owerUserId="123123"
+                loading={loading}
+                todoListData={todos}
+                isReadOnly={false}
+                onUpdate={onUpdateTodos}
+                onCreate={onCreateEmptyTodos}
+                onDelete={onDeleteTodos}
+                onSearch={onSearchTodos}
+            />
+        </div>
+    );
 };
 
 export default TodoContainer;
